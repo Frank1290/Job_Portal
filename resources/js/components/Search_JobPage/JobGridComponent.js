6ef1bcf09bf10c3a23f9ab6e95bf44171075784b
Frank1290 <joshifranklin@gmail.com>
2020-06-17 18:33:58 +0530

    feat: created job-grid component

---
+++ resources/js/components/Search_JobPage/JobGridComponent.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const JobGridComponent = ({ jobInfo }) => {
+    return (
+        <div className="job-lists">
+            <div className="logo-section" className="photo">
+                <img src="/images/cutting-edge1.png" />
+            </div>
+            <div className="job-list-section">
+                <div className="job-title">
+                    <span>{jobInfo.title}</span>
+                </div>
+                <div className="company-info">
+                    <span className="company-name">{jobInfo.company}</span>
+                    <span>Posted:{jobInfo.posted}</span>
+                </div>
+
+                <div className="company-info-details">
+                    <span className="location">{jobInfo.location}</span>
+
+                    <span className="salary">{jobInfo.salary}/Month</span>
+                    <span className="type">{jobInfo.type}</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+JobGridComponent.prototype = {
+    jobInfo: PropTypes.shape({
+        title: PropTypes.string,
+        company: PropTypes.string,
+        posted: PropTypes.string,
+        location: PropTypes.string,
+        salary: PropTypes.string,
+        type: PropTypes.string
+    })
+};
+export default JobGridComponent;
--- resources/js/components/Search_JobPage/SearchJobComponent.js
+++ resources/js/components/Search_JobPage/SearchJobComponent.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import axios from "axios";
+
+import JobGridComponent from "./JobGridComponent";
+
 export default class SearchJobComponent extends Component {
     constructor() {
         super();
         this.state = {
-            jobList: []
+            jobList: [],
+            obj: {
+                title: "Senior Marketing Officer,Generator",
+                company: "Shomka Agency",
+                posted: "January 6,2020",
+                location: "India, Mumbai",
+                salary: "$1260",
+                type: "Full-Time"
+            }
         };
     }
     componentDidMount() {
@@ -19,9 +30,14 @@ export default class SearchJobComponent extends Component {
     render() {
         return (
             <div>
-                {this.state.jobList.map(list => {
-                    return <h2>Title : {list.title}</h2>;
-                })}
+                <div>
+                    <JobGridComponent jobInfo={this.state.obj} />
+                </div>
+                <div>
+                    {this.state.jobList.map(list => {
+                        return <h2>Title : {list.title}</h2>;
+                    })}
+                </div>
             </div>
         );
     }
